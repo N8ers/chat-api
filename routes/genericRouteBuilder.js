c27a9fabc65e2fe45656eaf19c42971e486241a9
Nathan <nathanraysheryak@gmail.com>
2021-03-23 17:28:03 -0400

    genericRouteBuilder.js

---
+++ routes/genericRouteBuilder.js
@@ -0,0 +1,31 @@
+const knex = require('../config/config')
+
+async function buildGenericRoutes (routesToBuild, tableToQuery, router) {
+  if (routesToBuild.includes('GetAll')) {
+    console.log('getall')
+    router.get(`/`, async (req, res) => {
+      let result = await knex.select().from(tableToQuery);
+      res.send(result)
+    })
+  }
+
+  if (routesToBuild.includes('GetById')) {
+    router.get(`/:id`, async (req, res) => {
+      let result = await knex.select().from(tableToQuery).where({ id: req.params.id })
+      res.send(result)
+    })
+  }
+
+  if (routesToBuild.includes('DeleteById')) {
+    router.delete(`/:id`, async (req, res) => {
+      await knex(tableToQuery).where({ id: req.params.id }).del()
+      res.send('delete successful')
+    })
+  }
+
+  return router;
+}
+
+module.exports = {
+  buildGenericRoutes
+}
