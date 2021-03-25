2bc71a57b7d0d6b80d408677f3bec5ebf734aa7c
Nathan <nathanraysheryak@gmail.com>
2021-03-25 07:47:29 -0400

    setup conversationMember + messages w/ generic routes

--- routes/conversationMember.routes.js
+++ routes/conversationMember.routes.js
@@ -1,37 +1,17 @@
 const router = require('express').Router()
+const knex = require('../config/config')
 
 const { } = require('../controllers/conversationMember.controller')
+const { buildGenericRoutes } = require('./genericRouteBuilder');
 
-// get allConversationMembers
-router.get('/conversationMembers', async (req, res) => { 
-  let conversationMembers = await getAllConversationMembers()
-  res.send({ conversationMembers })
-})
+// generic routes
+const genericRoutesToBuild = ['GetAll', 'GetById', 'DeleteById']
+const tableToQuery = 'conversation_members'
+buildGenericRoutes(genericRoutesToBuild, tableToQuery, router)
 
-// get allConversationsByMember
-router.get('/conversationMembersByUserId', async (req, res) => {
-  let conversationMembers = await getAllConversationMembersByMember(req)
-  res.send({ conversationMembers })
-})
 
-// get allMembersByConversation
-router.get('/conversationMembersByConversationId', async (req, res) => { 
-  let conversationMembers = await getAllConversationMembersByConversation(req)
-  res.send({ conversationMembers })
-})
-
-// create memberConversation
-// this is confusing, but it adds userId to conversation (basically)
-router.post('/conversationMember', async (req, res) => {
-  let conversationMember = await createConversationMember(req)
-  res.send({ conversationMember })
-})
-
-// delete memberConversation
-// removes member from conversation
-router.delete('/conversationMember', async (req, res) => {
-  let conversationMember = await deleteConversationMember(req)
-  res.send({ conversationMember })
-})
+// create MemberConversation
+// router.post('/conversationMember', async (req, res) => {
+// })
 
 module.exports = router;
--- routes/message.routes.js
+++ routes/message.routes.js
@@ -1,41 +1,24 @@
 const router = require('express').Router()
+const knex = require('../config/config')
 
 const { } = require('../controllers/message.controller')
+const { buildGenericRoutes } = require('./genericRouteBuilder');
 
-// get Messages
-router.get('/messages', async (req, res) => {
-  let messages = await getAllMessages()
-  res.send(messages)
-})
-
-// get Messages by MessageId
-router.get('/messageId', async (req, res) => {
-  let message = await getAllMessagesById(req)
-  res.send(message)
-})
+// generic routes
+const genericRoutesToBuild = ['GetAll', 'GetById', 'DeleteById']
+const tableToQuery = 'messages'
+buildGenericRoutes(genericRoutesToBuild, tableToQuery, router)
 
 // get Messages by AuthorId
-router.get('/authorId', async (req, res) => {
-  let message = await getAllMessagesByAuthorId(req)
-  res.send(message)
-})
+// router.get('/authorId', async (req, res) => {
+// })
 
 // get Messages by ConversationId
-router.get('/conversationId', async (req, res) => {
-  let message = await getAllMessagesByConversationId(req)
-  res.send(message)
-})
+// router.get('/conversationId', async (req, res) => {
+// })
 
 // create Message
-router.post('/message', async (req, res) => {
-  let result = await createMessage(req)
-  res.send({ message: result })
-})
-
-// delete Message by id
-router.delete('/message', async (req, res) => { 
-  let result = await deleteMessageById(req)
-  res.send({ message: result })
-})
+// router.post('/message', async (req, res) => {
+// })
 
 module.exports = router;
