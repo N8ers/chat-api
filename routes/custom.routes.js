e7996be0283b4c5c316a391010f7fd394515979c
Nathan <nathanraysheryak@gmail.com>
2021-04-01 06:57:11 -0400

    modify `/init/:user_id` to return only 'friend.id/username'

--- routes/custom.routes.js
+++ routes/custom.routes.js
@@ -4,11 +4,17 @@ const knex = require('../config/config')
 router.get('/init/:user_id', async (req, res) => {
   const { user_id } = req.params
   const conversations = await 
-    knex.select('*')
+    knex.select(
+      'conversation_members.id',
+      'users.id as friendId',
+      'users.username as friendUsername',
+      'conversations.name as conversationName',
+      'conversations.id as conversationId'
+      )
       .from('conversation_members')
       .join('conversations', 'conversations.id', '=', 'conversation_members.conversation_id')
       .join('users', 'users.id', '=', 'conversation_members.user_id')
-      .where('conversation_members.user_id', user_id)
+      .whereNot('conversation_members.user_id', user_id)
   
   res.json(conversations)
 })
